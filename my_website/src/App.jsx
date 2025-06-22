4b404dbcd7ebc19e2019f8503c4c07246ec9026a
med <jassimssalim20@gmail.com>
2025-06-22 17:12:12 +0800

    Adding all files

---
+++ my_website/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
+function App() {
+
+  return (
+      <> 
+        <Header/>
+
+        <main className='main'> 
+          <Home />
+          <About />
+        </main>
+      </>
+      
+    
+    )
+}
+
+export default App
---
+++ my_website/src/components/home/Social.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+      <div className="home__social">
+          <a href="test" className="home__social-icon" target='_blank'>
+               <i class="uil uil-instagram"></i>
+          </a>
+          <a href="test" className="home__social-icon" target='_blank'>
+          <i class="uil uil-linkedin-alt"></i>
+          </a>
+
+          <a href="test" className="home__social-icon" target='_blank'>
+             <i class="uil uil-github-alt"></i>    
+          </a>
+
+          <a href="test" className="home__social-icon" target='_blank'>
+             <i class="uil uil-arrow"></i>   
+          </a>
+
+    
+
+      </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  )
+}
+
+export default Social
